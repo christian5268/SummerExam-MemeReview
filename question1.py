731a59c9970ca88c87cce8d03c10f6462b1ac2d9
MeganC123 <16MCollins@stjosephsrush.ie>
2018-05-17 09:42:04 +0100

    Create question1.py

---
+++ question1.py
@@ -0,0 +1,20 @@
+while True:
+
+    mountain=input("what way do muslims face while praying a)North b) The ocean or c)the ka'ba")
+
+    if mountain=="a":
+        print("sorry that is the wrong answer")
+        print("try again")
+
+
+
+    elif mountain=="b":
+        print("sorry that is the wrong answer")
+        print("try again")
+
+
+
+    elif mountain=="c":
+        print("correct you now move onto the next level")
+        
+
