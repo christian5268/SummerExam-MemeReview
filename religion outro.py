d4f4b4bb26e57c6a3a284c119778646aa6262467
Christian Jones <16CJones1@stjosephsrush.ie>
2018-05-24 09:25:26 +0100

    Religion outro

---
+++ religion outro.py
@@ -0,0 +1,9 @@
+from time import sleep 
+
+print("Thank you for playing our religion game we hope you learned something from this game")
+sleep(2)
+print("if you have any questions or any changes you want let us know just give us some feedback")
+sleep(2)
+
+
+
