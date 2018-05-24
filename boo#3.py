be602d612d187aec743f10ec768b838369a60f38
Boo Griffith <16egriffith@stjosephsrush.com>
2018-05-24 09:30:43 +0100

    Create boo#3.py

---
+++ boo#3.py
@@ -0,0 +1,65 @@
+#written by boo
+#all comments are written by boo
+
+from time import sleep #control to pause the code 
+
+print("You all of a sudden are faced with a giant mountain")#this is a print statement
+print("you have to get to the top but before you do that answer this question... ")#this is a print statement
+sleep(1)
+
+#written by boo  
+print("Once you have done your first few steps you are next going to have to move a bolder that is in your way,")#this is a print statement
+print("Another question is most defenitly on it's way...")#this is a print statement
+sleep(1)#pauses the code for a second(s)
+
+#written by boo
+print("The questions are going to keep getting harder the more you play so be prepared..."#this is a print statement)
+sleep(1)#pauses the code for a second(s)
+
+#written by boo 
+print("Since you have been so good at answering your tricky questions so far you are given a helping hand,")#this is a print statement
+print("which is...")#this is a print statement
+sleep(1)#pauses the code for a second(s)
+print("You get to now get closer and closer to the top of the mountain without any question.")#this is a print statement
+
+#written by Boo
+
+while True:
+    q7=input("Who were the 4 gospel writers? a) Mathew, Malcom, Jonathan, Henry  b) Luke, Matthew, John, Mark  c) Sarah, Mark, Ethan, Meghan")#this is a print statement
+    q7=q7.lower()
+
+
+    if q7=="a":
+		print("That is incorrect ")#this is a print statement
+		print("sorry you'll have to try again '")#this is a print statement
+		print (" you will now restart the question ")#this is a print statement
+
+    elif q7=="b":
+		print (" well done that is the right answer")#this is a print statement
+		print("you can now move on a few steps")#this is a print statement
+		break
+
+    elif q7=="c":
+        print ("that is incorrect")#this is a print statement
+        print (" you will now restart the question ")#this is a print statement
+
+print("You have come to an end of your journey, you have 1 more question to get to the top")#this is a print statement
+
+while True:
+      q8=input("Where is a hijab worn on a womens body?  a) The head  b) The shoulders  c) The waist")
+      q8=q8.lower()
+
+      
+      if q8=="a"():
+		print (" well done that is the right answer")#this is a print statement
+		print("you can now move on a few steps")#this is a print statement
+		break
+
+      elif q8=="b"():
+        print ("that is incorrect")#this is a print statement
+        print (" you will now restart the question ")#this is a print statement
+
+      elif q8=="c"():
+		print("That is incorrect ")#this is a print statement
+		print("sorry you'll have to try again '")#this is a print statement
+		print (" you will now restart the question ")#this is a print statement
