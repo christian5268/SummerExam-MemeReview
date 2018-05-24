bfe9d7dc65870bbae06aa399dc46c76076113f8d
AdamIreland <16AFerguson@stjosephsrush.com>
2018-05-24 09:42:17 +0100

    Update Intro.py

--- Intro.py
+++ Intro.py
@@ -1,68 +1,69 @@
-from time import sleep
+from time import sleep     #Adams Code
 
-print("Hey, welcome to our game!")
-sleep(1)
-print("You're probably wondering what this game is about. Well this game is about learning more about religions all around the world.")
-sleep(3)
-print("You can be asked any question about any religion.")
-sleep(2)
-print("If you are interested by religion in anyway this game will be perfect for you!")
-sleep(3)
-print("I will begin by giving you some example questions so you know what to expect.")
-sleep(3)
-While True:
-    print("What is the town that Jesus was born in?")
-    question1=input("a) Bethelhem b) Nazareth c) River Jordan  (Type in the letter corresponding to your answer) ")
+print("Hey, welcome to our game!")    #Adams Code
+sleep(1)    #Adams Code
+print("You're probably wondering what this game is about. Well this game is about learning more about religions all around the world.")    #Adams Code
+sleep(3)    #Adams Code
+print("You can be asked any question about any religion.")    #Adams Code
+sleep(2)    #Adams Code
+print("If you are interested by religion in anyway this game will be perfect for you!")    #Adams Code
+sleep(3)    #Adams Code
+print("I will begin by giving you some example questions so you know what to expect.")    #Adams Code
+sleep(3)    #Adams Code
+While True:    #Adams Code
+    print("What is the town that Jesus was born in?")    #Adams Code
+    question1=input("a) Bethelhem b) Nazareth c) River Jordan  (Type in the letter corresponding to your answer) ")    #Adams Code
 
-    if question1 =="a":
-        print("This is the correct answer! Hooray!")
-        sleep("On to the next question")
-        break
+    if question1 =="a":    #Adams Code
+        print("This is the correct answer! Hooray!")    #Adams Code
+        sleep("On to the next question")    #Adams Code
+        break    #Adams Code
     
-    elif question1 =="b"
-        print("Sorry but this isnt correct")
-        sleep(1)
-        print("Try again!")
+    elif question1 =="b"    #Adams Code
+        print("Sorry but this isnt correct")    #Adams Code
+        sleep(1)    #Adams Code
+        print("Try again!")    #Adams Code
         
-    elif question1 =="c"
-        print("Wrong, sorry!")
-        sleep(1)
-        print("Try again!")
+    elif question1 =="c"    #Adams Code
+        print("Wrong, sorry!")    #Adams Code
+        sleep(1)    #Adams Code
+        print("Try again!")    #Adams Code
         
-While True:
-    print("What is Jesus' mothers name?")
-    sleep(1)
-    question2=input("a)Mary b)Joe c)Ciara")
+While True:    #Adams Code
+    print("What is Jesus' mothers name?")    #Adams Code
+    sleep(1)    #Adams Code
+    question2=input("a)Mary b)Joe c)Ciara")    #Adams Code
     
-    if question2 =="a"
-        print("Correct! Next question")
-        break
+    if question2 =="a"    #Adams Code
+        print("Correct! Next question")    #Adams Code
+        break    #Adams Code
     
-    elif question2 == "b"
-        print("Wrong! Try again.")
+    elif question2 == "b"    #Adams Code
+        print("Wrong! Try again.")    #Adams Code
+            
+    elif question2 =="c"    #Adams Code
+        print("Wrong...")    #Adams Code
+        sleep(1)    #Adams Code
+        print("Go back and try again!")    #Adams Code
+        sleep(1)    #Adams Code
+        print("I'll give you a hint rhymes with scary.")    #Adams Code
         
-    elif question2 =="c"
-        print("Wrong...")
-        sleep(1)
-        print("Go back and try again!")
-        sleep(1)
-        print("I'll give you a hint rhymes with scary.")
+While True:    #Adams Code
+    print("What is the river Jesus was born in?")    #Adams Code
+    question3=input("a) River Jordan b) Amazonian River c) Uganda")    #Adams Code
+    
+    if question3 =="a"    #Adams Code
+        print("Correct, well done!")    #Adams Code
+        sleep(1)    #Adams Code
+        break    #Adams Code
+    
+    elif question3 =="b"    #Adams Code
+        print("Wrong!")    #Adams Code
+        sleep(1)    #Adams Code
+        print("Try AGAIN!")    #Adams Code
+    
+    elif question3 =="c"    #Adams Code
+        print("WRONG AGAIN")    #Adams Code
+        sleep(1)    #Adams Code
+        print("HOW COULD IT EVEN BE THAT?")    #Adams Code
         
-While True:
-    print("What is the river Jesus was born in?")
-    question3=input("a) River Jordan b) Amazonian River c) Uganda")
-    
-    if question3 =="a"
-        print("Correct, well done!")
-        sleep(1)
-        break
-    
-    elif question3 =="b"
-        print("Wrong!")
-        sleep(1)
-        print("Try AGAIN!")
-    
-    elif question3 =="c"
-        print("WRONG AGAIN")
-        sleep(1)
-        print("HOW COULD IT EVEN BE THAT?")
