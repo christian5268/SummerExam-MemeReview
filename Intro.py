9216655574a8ac456c5d96e95e4e389e4e6e7efc
AdamIreland <16AFerguson@stjosephsrush.com>
2018-05-24 09:16:12 +0100

    Create Intro.py

---
+++ Intro.py
@@ -0,0 +1,68 @@
+from time import sleep
+
+print("Hey, welcome to our game!")
+sleep(1)
+print("You're probably wondering what this game is about. Well this game is about learning more about religions all around the world.")
+sleep(3)
+print("You can be asked any question about any religion.")
+sleep(2)
+print("If you are interested by religion in anyway this game will be perfect for you!")
+sleep(3)
+print("I will begin by giving you some example questions so you know what to expect.")
+sleep(3)
+While True:
+    print("What is the town that Jesus was born in?")
+    question1=input("a) Bethelhem b) Nazareth c) River Jordan  (Type in the letter corresponding to your answer) ")
+
+    if question1 =="a":
+        print("This is the correct answer! Hooray!")
+        sleep("On to the next question")
+        break
+    
+    elif question1 =="b"
+        print("Sorry but this isnt correct")
+        sleep(1)
+        print("Try again!")
+        
+    elif question1 =="c"
+        print("Wrong, sorry!")
+        sleep(1)
+        print("Try again!")
+        
+While True:
+    print("What is Jesus' mothers name?")
+    sleep(1)
+    question2=input("a)Mary b)Joe c)Ciara")
+    
+    if question2 =="a"
+        print("Correct! Next question")
+        break
+    
+    elif question2 == "b"
+        print("Wrong! Try again.")
+        
+    elif question2 =="c"
+        print("Wrong...")
+        sleep(1)
+        print("Go back and try again!")
+        sleep(1)
+        print("I'll give you a hint rhymes with scary.")
+        
+While True:
+    print("What is the river Jesus was born in?")
+    question3=input("a) River Jordan b) Amazonian River c) Uganda")
+    
+    if question3 =="a"
+        print("Correct, well done!")
+        sleep(1)
+        break
+    
+    elif question3 =="b"
+        print("Wrong!")
+        sleep(1)
+        print("Try AGAIN!")
+    
+    elif question3 =="c"
+        print("WRONG AGAIN")
+        sleep(1)
+        print("HOW COULD IT EVEN BE THAT?")
