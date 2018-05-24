e3d2703ea690ee608be32ce961256a1055a8b037
Christian Jones <16CJones1@stjosephsrush.ie>
2018-05-24 09:12:08 +0100

    religionquestion2

---
+++ religion question2.py
@@ -0,0 +1,21 @@
+while True:
+
+    question=input("Chose a miracle jesus has performed a)Cured a blind man b) stopped aliens  c)stopped a metor")
+
+    if question=="c":
+        print("sorry that is the wrong answer")
+        print("try again")
+
+
+
+    elif question=="b":
+        print("sorry that is the wrong answer")
+        print("try again")
+
+
+
+    elif question=="a":
+        print("correct you now move onto the next level")
+        
+
+
